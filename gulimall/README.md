d5962fa2384b5b43109e3dd183457e5f0179f888
lpruoyu <2975064528@qq.com>
2024-06-19 19:19:41 +0800

    新增搜索服务（ElasticSearch）并整合测试完成

--- gulimall/README.md
+++ gulimall/README.md
@@ -3,3 +3,10 @@
 
 # CSDN
 https://blog.csdn.net/weixin_44018671/article/details/127935629
+
+# 分享资源
+https://pan.baidu.com/s/1hkL-oho86RA-XHdFnoLPtA?=xwee 大厂学苑
+
+https://pan.baidu.com/share/init?surl=mvo0u9XSQHsMjrDwE854eg 提取码ehak 大厂学苑
+
+https://pan.baidu.com/s/1igIlI91L4FQZ0CG9Kgh9zg?pwd=yyds 提取码：yyds  谷粒商城
--- renren-fast-vue/src/views/modules/product/attrupdate.vue
+++ renren-fast-vue/src/views/modules/product/attrupdate.vue
@@ -4,43 +4,19 @@
       <el-col :span="16">
         <el-card class="box-card">
           <el-tabs tab-position="left" style="width:98%">
-            <el-tab-pane
-              :label="group.attrGroupName"
-              v-for="(group,gidx) in dataResp.attrGroups"
-              :key="group.attrGroupId"
-            >
+            <el-tab-pane :label="group.attrGroupName" v-for="(group, gidx) in dataResp.attrGroups"
+              :key="group.attrGroupId">
               <!-- 遍历属性,每个tab-pane对应一个表单，每个属性是一个表单项  spu.baseAttrs[0] = [{attrId:xx,val:}]-->
               <el-form ref="form" :model="dataResp">
-                <el-form-item
-                  :label="attr.attrName"
-                  v-for="(attr,aidx) in group.attrs"
-                  :key="attr.attrId"
-                >
-                  <el-input
-                    v-model="dataResp.baseAttrs[gidx][aidx].attrId"
-                    type="hidden"
-                    v-show="false"
-                  ></el-input>
-                  <el-select
-                    v-model="dataResp.baseAttrs[gidx][aidx].attrValues"
-                    :multiple="attr.valueType == 1"
-                    filterable
-                    allow-create
-                    default-first-option
-                    placeholder="请选择或输入值"
-                  >
-                    <el-option
-                      v-for="(val,vidx) in attr.valueSelect.split(';')"
-                      :key="vidx"
-                      :label="val"
-                      :value="val"
-                    ></el-option>
+                <el-form-item :label="attr.attrName" v-for="(attr, aidx) in group.attrs" :key="attr.attrId">
+                  <el-input v-model="dataResp.baseAttrs[gidx][aidx].attrId" type="hidden" v-show="false"></el-input>
+                  <el-select v-model="dataResp.baseAttrs[gidx][aidx].attrValues" :multiple="attr.valueType == 1"
+                    filterable allow-create default-first-option placeholder="请选择或输入值">
+                    <el-option v-for="(val, vidx) in attr.valueSelect.split(';')" :key="vidx" :label="val"
+                      :value="val"></el-option>
                   </el-select>
-                  <el-checkbox
-                    v-model="dataResp.baseAttrs[gidx][aidx].showDesc"
-                    :true-label="1"
-                    :false-label="0"
-                  >快速展示</el-checkbox>
+                  <el-checkbox v-model="dataResp.baseAttrs[gidx][aidx].showDesc" :true-label="1"
+                    :false-label="0">快速展示</el-checkbox>
                 </el-form-item>
               </el-form>
             </el-tab-pane>
@@ -72,7 +48,7 @@ export default {
   },
   computed: {},
   methods: {
-    clearData(){
+    clearData() {
       this.dataResp.attrGroups = [];
       this.dataResp.baseAttrs = [];
       this.spuAttrsMap = {};
@@ -105,23 +81,26 @@ export default {
         //先对表单的baseAttrs进行初始化
         data.data.forEach(item => {
           let attrArray = [];
-          item.attrs.forEach(attr => {
-            let v = "";
-            if (_this.spuAttrsMap["" + attr.attrId]) {
-              v = _this.spuAttrsMap["" + attr.attrId].attrValue.split(";");
-              if (v.length == 1) {
-                v = v[0] + "";
+          if (item.attrs != null) {
+            item.attrs.forEach(attr => {
+              let v = "";
+              if (_this.spuAttrsMap["" + attr.attrId]) {
+                v = _this.spuAttrsMap["" + attr.attrId].attrValue.split(";");
+                if (v.length == 1 && attr.valueType == 0) {
+                  v = v[0] + ''
+                }
               }
-            }
-            attrArray.push({
-              attrId: attr.attrId,
-              attrName: attr.attrName,
-              attrValues: v,
-              showDesc: _this.spuAttrsMap["" + attr.attrId]
-                ? _this.spuAttrsMap["" + attr.attrId].quickShow
-                : attr.showDesc
+              attrArray.push({
+                attrId: attr.attrId,
+                attrName: attr.attrName,
+                attrValues: v,
+                showDesc: _this.spuAttrsMap["" + attr.attrId]
+                  ? _this.spuAttrsMap["" + attr.attrId].quickShow
+                  : attr.showDesc
+              });
             });
-          });
+          }
+
           this.dataResp.baseAttrs.push(attrArray);
         });
         this.dataResp.attrGroups = data.data;
@@ -171,12 +150,12 @@ export default {
         .catch((e) => {
           this.$message({
             type: "info",
-            message: "已取消修改"+e
+            message: "已取消修改" + e
           });
         });
     }
   },
-  created() {},
+  created() { },
   activated() {
     this.clearData();
     this.getQueryParams();
